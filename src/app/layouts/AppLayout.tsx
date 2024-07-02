f9ff4e673cfbf72532103f56ac4d76568be9caa8
Liang Li <liang.li@linbit.com>
2024-07-02 21:05:05 +0800

    fix: dropdown vibrates

--- src/app/layouts/AppLayout.tsx
+++ src/app/layouts/AppLayout.tsx
@@ -257,7 +257,7 @@ const AppLayout: React.FunctionComponent<IAppLayout> = ({ children }) => {
 
         {!normalWithoutAuth && (
           <PageHeaderToolsItem>
-            <Dropdown menu={menu} placement="bottom">
+            <Dropdown menu={menu} placement="bottomLeft">
               <Avatar size={40} style={{ backgroundColor: '#f7a75c', color: '#1e2939', cursor: 'pointer' }}>
                 {authInfo.username?.charAt(0).toUpperCase() || 'A'}
               </Avatar>
