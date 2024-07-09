c8c4378d8665fc41e5e2595f21f880b56592350b
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2024-07-09 15:37:55 +0200

    add release script

---
+++ scripts/release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+die() {
+	echo >&2 "$1"
+	exit 1
+}
+
+version=$1
+[ -z "$version" ] && die "Usage: $0 <version>"
+
+export EMAIL="$(git config --get user.email)"
+export NAME="$(git config --get user.name)"
+
+version_and_release="${version}-1"
+
+rpmdev-bumpspec -n "$version_and_release" \
+	-c "New upstream release" \
+	-u "$NAME <$EMAIL>" \
+	linstor-gui.spec
+
+dch -v "$version_and_release" \
+	-u "medium" \
+	"New upstream release" \
+	&& dch -r ""
+
+git add linstor-gui.spec debian/changelog
+git commit --message "Release v$version"
+git tag "v$version"
+
+git --no-pager show
