c3840da460914928a38399f88c1bff3191f368dc
Liang Li <liang.li@linbit.com>
2024-08-28 17:54:17 +0800

    feat: create/edit passphrase

--- src/app/features/settings/passphrase.ts
+++ src/app/features/settings/passphrase.ts
@@ -1,4 +1,21 @@
-import { patch } from '../requests';
+import { patch, post, put } from '../requests';
+
+const createPassphrase = (passphrase: string) => {
+  return post('/v1/encryption/passphrase', {
+    body: {
+      new_passphrase: passphrase,
+    },
+  });
+};
+
+const editPassphrase = (passphrase: string, old_passphrase: string) => {
+  return put('/v1/encryption/passphrase', {
+    body: {
+      new_passphrase: passphrase,
+      old_passphrase: old_passphrase,
+    },
+  });
+};
 
 const enterPassPhrase = (passphrase: string) => {
   return patch('/v1/encryption/passphrase', {
@@ -6,4 +23,4 @@ const enterPassPhrase = (passphrase: string) => {
   });
 };
 
-export { enterPassPhrase };
+export { enterPassPhrase, createPassphrase, editPassphrase };
--- src/app/pages/Settings/components/Passphrase/index.tsx
+++ src/app/pages/Settings/components/Passphrase/index.tsx
@@ -1,78 +1,151 @@
-import React, { useState, useCallback } from 'react';
-import { Input, Button, message } from 'antd';
+import React from 'react';
+import { Input, Button, message, Form, FormProps, Radio } from 'antd';
 import styled from '@emotion/styled';
 import { useMutation } from '@tanstack/react-query';
-import { enterPassPhrase } from '@app/features/settings/passphrase';
+import { createPassphrase, editPassphrase } from '@app/features/settings/passphrase';
 
 const Wrapper = styled.div`
   padding: 2em 0;
   width: 20em;
 `;
 
-const Label = styled.span`
-  margin-bottom: 1em;
+const Label = styled.div`
+  margin-left: 4em;
+  margin-bottom: 2em;
 `;
 
+type FieldType = {
+  confirm: string;
+  password: string;
+  mode: 'create' | 'edit';
+  old: string;
+};
+
 const Passphrase: React.FC = () => {
-  const [passphrase, setPassphrase] = useState('');
   const [messageApi, contextHolder] = message.useMessage();
 
-  const enterPassphrase = useMutation({
-    mutationKey: ['enterPassPhrase'],
+  const [form] = Form.useForm();
+  const mode = Form.useWatch('mode', form);
+
+  const onFinish = (values: FieldType) => {
+    console.log('Success:', values);
+    if (values.mode === 'edit') {
+      editPassphraseMutation.mutate({ passphrase: values.password, old: values.old });
+    } else {
+      createPassphraseMutation.mutate(values.password);
+    }
+  };
+
+  const validateConfirmPassword = (_: any, value: FieldType) => {
+    if (value && value !== form.getFieldValue('password')) {
+      return Promise.reject(new Error('The two passphrases not match!'));
+    }
+    return Promise.resolve();
+  };
+
+  const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const createPassphraseMutation = useMutation({
+    mutationKey: ['createPassphrase'],
     mutationFn: (passphrase: string) => {
-      return enterPassPhrase(passphrase);
-    },
-    onMutate: () => {
-      messageApi.open({
-        type: 'info',
-        content: 'Validating passphrase...',
-      });
+      return createPassphrase(passphrase);
     },
     onSuccess: () => {
       messageApi.open({
         type: 'success',
-        content: 'Unlock successfully.',
+        content: 'Saved successfully.',
       });
     },
     onError: () => {
       messageApi.open({
         type: 'error',
-        content: 'Failed to unlock.',
+        content: 'Failed to save.',
       });
     },
   });
 
-  const handleSave = useCallback(() => {
-    if (!passphrase) {
+  const editPassphraseMutation = useMutation({
+    mutationKey: ['editPassphrase'],
+    mutationFn: ({ passphrase, old }: { passphrase: string; old: string }) => {
+      return editPassphrase(passphrase, old);
+    },
+    onSuccess: () => {
+      messageApi.open({
+        type: 'success',
+        content: 'Saved successfully.',
+      });
+    },
+    onError: () => {
       messageApi.open({
         type: 'error',
-        content: 'Please enter the passphrase.',
+        content: 'Failed to save.',
       });
-      return;
-    } else {
-      console.log('Save', passphrase);
-      enterPassphrase.mutate(passphrase);
-    }
-  }, [passphrase, messageApi, enterPassphrase]);
+    },
+  });
 
   return (
     <>
       {contextHolder}
       <Wrapper>
-        <Label>LINSTOR Passphrase</Label>
-        <Input.Password
-          value={passphrase}
-          onChange={(e) => {
-            setPassphrase(e.target.value);
-          }}
-          aria-label="pass-phrase"
-          type="password"
-          width={100}
-        />
+        <Form
+          form={form}
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 18 }}
+          style={{ minWidth: 600 }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          initialValues={{ mode: 'create' }}
+        >
+          <Label>LINSTOR Passphrase</Label>
+          <Form.Item label="Create/Edit" name="mode">
+            <Radio.Group
+              onChange={() => {
+                form.resetFields(['old', 'password', 'confirm']);
+              }}
+            >
+              <Radio.Button value="create"> Create </Radio.Button>
+              <Radio.Button value="edit"> Edit </Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+          {mode === 'edit' ? (
+            <Form.Item
+              label="Old passphrase"
+              name="old"
+              rules={[{ required: true, message: 'Please input your old passphrase!' }]}
+            >
+              <Input.Password />
+            </Form.Item>
+          ) : null}
+          <Form.Item<FieldType>
+            label="Passphrase"
+            name="password"
+            rules={[{ required: true, message: 'Please input your passphrase!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item<FieldType>
+            name="confirm"
+            label="Confirm"
+            dependencies={['password']}
+            rules={[
+              { required: true, message: 'Please confirm your passphrase!' },
+              { validator: validateConfirmPassword },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </Wrapper>
-      <Button type="primary" onClick={handleSave}>
-        Unlock
-      </Button>
     </>
   );
 };
